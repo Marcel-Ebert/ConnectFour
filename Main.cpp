36c2e63a91b97d3268cce0d78e66ebfd6f6ad934
Marcel Ebert <marcel@wolfgang-ebert.de>
2018-06-10 17:15:04 +0200

    refactored methods extracted variables

--- Main.cpp
+++ Main.cpp
@@ -94,23 +94,29 @@ void drawCS()
 
 }
 
-void drawSeg(GLfloat height)
-{
-	glm::mat4 Save = Model;
+const int PLAYER = 1;
+const int NPC = 2;
 
-	Model = glm::translate(Model, glm::vec3(0, height / 2, 0));
-	Model = glm::scale(Model, glm::vec3(height / 6, height / 2, height / 6));
-	sendMVP();
-	drawSphere(100, 100);
-	Model = Save;
-
-}
+const int BOARD_SIZE = 8;
+int boardArray[BOARD_SIZE][BOARD_SIZE];
 
 GLuint VertexArrayIDBoard;
 GLuint VertexArrayIDChip;
 GLuint yellowTexture;
 GLuint blueTexture;
 GLuint woodTexture;
+
+std::vector<glm::vec3> boardVertices;
+std::vector<glm::vec2> boardUVs;
+std::vector<glm::vec3> boardNormals;
+
+std::vector<glm::vec3> chipVertices;
+std::vector<glm::vec2> chipUvs;
+std::vector<glm::vec3> chipNormals;
+
+GLuint vertexBuffer;
+GLuint normalBuffer;
+GLuint textureBuffer;
 
 void drawChip(glm::vec3 position, bool player, GLsizei arraysize) {
 	glm::mat4 Save = Model;
@@ -134,7 +140,7 @@ void drawChip(glm::vec3 position, bool player, GLsizei arraysize) {
 	Model = Save;
 }
 
-void drawBoard(GLsizei arraysize) {
+void drawBoard() {
 	glm::mat4 Save = Model;
 
 	Model = glm::translate(Model, glm::vec3(8, 0, 8));
@@ -149,36 +155,106 @@ void drawBoard(GLsizei arraysize) {
 	glUniform1i(glGetUniformLocation(programID, "myTextureSampler"), 0); // 1 integer wert
 
 	glBindVertexArray(VertexArrayIDBoard);
-	glDrawArrays(GL_TRIANGLES, 0, arraysize);
+	glDrawArrays(GL_TRIANGLES, 0, boardVertices.size());
 }
 
-int boardSize = 0;
-int **boardArray;
-
 void createArrayForBoard() {
-	boardArray = new int*[boardSize];
-	for (int i = 0; i < boardSize; ++i) {
-		boardArray[i] = new int[boardSize];
+	for (int i = 0; i < BOARD_SIZE; i++) {
+		for (int j = 0; j < BOARD_SIZE; j++) {
+			boardArray[i][j] = 0;
+		}
 	}
 }
 
-void drawChipsInBoardArray(GLsizei chipVerticesSize) {
-	for (int i = 0; i < boardSize; i++) {
-		for (int j = 0; j < boardSize; j++) {
-			if (boardArray[i][j] == 1) {
-				drawChip(glm::vec3(1 + i * 2, 0, 1 + j * 2), true, chipVerticesSize);
-			} else if (boardArray[i][j] == 2) {
-				drawChip(glm::vec3(1 + i * 2, 0, 1 + j * 2), false, chipVerticesSize);
+void drawChipsInBoardArray() {
+	for (int i = 0; i < BOARD_SIZE; i++) {
+		for (int j = 0; j < BOARD_SIZE; j++) {
+			if (boardArray[i][j] == PLAYER) {
+				drawChip(glm::vec3(1 + i * 2, 0, 1 + j * 2), true, chipVertices.size());
+			} else if (boardArray[i][j] == NPC) {
+				drawChip(glm::vec3(1 + i * 2, 0, 1 + j * 2), false, chipVertices.size());
 			}
 		}
 	}
 }
 
 void deleteArrayForBoard() {
-	for (int i = 0; i < boardSize; ++i) {
+	for (int i = 0; i < BOARD_SIZE; ++i) {
 		delete[] boardArray[i];
 	}
 	delete[] boardArray;
+}
+
+
+void loadBoardToMemory() {
+	bool res = loadOBJ("ConnectFourBoard.obj", boardVertices, boardUVs, boardNormals);
+
+	glGenVertexArrays(1, &VertexArrayIDBoard); // id wird an ein in methode erstelltes vbo object zugewiesen
+	glBindVertexArray(VertexArrayIDBoard); // id wird an Objekt gebunden
+
+	glGenBuffers(1, &vertexBuffer); // vbo bekommt buffer zugewiesen
+	glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer); // buffer von typ arraybuffer wird gebunden
+	glBufferData(GL_ARRAY_BUFFER, boardVertices.size() * sizeof(glm::vec3), // groesse des Buffers in Byte
+		&boardVertices[0],
+		GL_STATIC_DRAW); //kein dynamischer Inhalt - statisch gezeichnet
+
+	glEnableVertexAttribArray(0); // 0 ist Standard -> Position in meisten Shadern
+	glVertexAttribPointer(0, // Position in shader
+		3, // Datenformat (3 Werte)
+		GL_FLOAT, // Typ
+		GL_FALSE, // normalisiert?
+		0, // sind Punkte direkt hintereinander gespeichert? ja-0 
+		(void*)0); // kann beliebigen Datentyp annehmen
+
+	glGenBuffers(1, &normalBuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, normalBuffer);
+	glBufferData(GL_ARRAY_BUFFER, boardNormals.size() * sizeof(glm::vec3), &boardNormals[0], GL_STATIC_DRAW);
+
+	glEnableVertexAttribArray(2); // ist im Shader so hinterlegt
+	glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+	
+	glGenBuffers(1, &textureBuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, textureBuffer);
+	glBufferData(GL_ARRAY_BUFFER, boardUVs.size() * sizeof(glm::vec2), &boardUVs[0], GL_STATIC_DRAW);
+
+	glEnableVertexAttribArray(1); // ist im Shader so hinterlegt
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, (void*)0);
+
+}
+
+
+void loadChipToMemory() {
+	bool res = loadOBJ("Chip.obj", chipVertices, chipUvs, chipNormals);
+	glGenVertexArrays(1, &VertexArrayIDChip); // id wird an ein in methode erstelltes vbo object zugewiesen
+	glBindVertexArray(VertexArrayIDChip); // id wird an Objekt gebunden
+
+	glGenBuffers(1, &vertexBuffer); // vbo bekommt buffer zugewiesen
+	glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer); // buffer von typ arraybuffer wird gebunden
+	glBufferData(GL_ARRAY_BUFFER, chipVertices.size() * sizeof(glm::vec3), // groesse des Buffers in Byte
+		&chipVertices[0],
+		GL_STATIC_DRAW); //kein dynamischer Inhalt - statisch gezeichnet
+
+	glEnableVertexAttribArray(0); // 0 ist Standard -> Position in meisten Shadern
+	glVertexAttribPointer(0, // Position in shader
+		3, // Datenformat (3 Werte)
+		GL_FLOAT, // Typ
+		GL_FALSE, // normalisiert?
+		0, // sind Punkte direkt hintereinander gespeichert? ja-0 
+		(void*)0); // kann beliebigen Datentyp annehmen
+
+	glGenBuffers(1, &normalBuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, normalBuffer);
+	glBufferData(GL_ARRAY_BUFFER, chipNormals.size() * sizeof(glm::vec3), &chipNormals[0], GL_STATIC_DRAW);
+
+	glEnableVertexAttribArray(2); // ist im Shader so hinterlegt
+	glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+;
+	glGenBuffers(1, &textureBuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, textureBuffer);
+	glBufferData(GL_ARRAY_BUFFER, chipUvs.size() * sizeof(glm::vec2), &chipUvs[0], GL_STATIC_DRAW);
+
+	glEnableVertexAttribArray(1); // ist im Shader so hinterlegt
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, (void*)0);
 }
 
 int main(void)
@@ -239,88 +315,24 @@ int main(void)
 
 	glEnable(GL_DEPTH_TEST);
 	glDepthFunc(GL_LESS);
-
-	std::vector<glm::vec3> vertices;
-	std::vector<glm::vec2> uvs;
-	std::vector<glm::vec3> normals;
-
-	bool res = loadOBJ("ConnectFourBoard.obj", vertices, uvs, normals);
 	
-	glGenVertexArrays(1, &VertexArrayIDBoard); // id wird an ein in methode erstelltes vbo object zugewiesen
-	glBindVertexArray(VertexArrayIDBoard); // id wird an Objekt gebunden
+	loadBoardToMemory();
 
-	GLuint vertexBuffer;
-	glGenBuffers(1, &vertexBuffer); // vbo bekommt buffer zugewiesen
-	glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer); // buffer von typ arraybuffer wird gebunden
-	glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(glm::vec3), // groesse des Buffers in Byte
-		&vertices[0], 
-		GL_STATIC_DRAW); //kein dynamischer Inhalt - statisch gezeichnet
-	
-	glEnableVertexAttribArray(0); // 0 ist Standard -> Position in meisten Shadern
-	glVertexAttribPointer(0, // Position in shader
-		3, // Datenformat (3 Werte)
-		GL_FLOAT, // Typ
-		GL_FALSE, // normalisiert?
-		0, // sind Punkte direkt hintereinander gespeichert? ja-0 
-		(void*)0); // kann beliebigen Datentyp annehmen
-
-	GLuint normalBuffer;
-	glGenBuffers(1, &normalBuffer);
-	glBindBuffer(GL_ARRAY_BUFFER, normalBuffer);
-	glBufferData(GL_ARRAY_BUFFER, normals.size() * sizeof(glm::vec3), &normals[0], GL_STATIC_DRAW);
-	
-	glEnableVertexAttribArray(2); // ist im Shader so hinterlegt
-	glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
-
-	GLuint textureBuffer;
-	glGenBuffers(1, &textureBuffer);
-	glBindBuffer(GL_ARRAY_BUFFER, textureBuffer);
-	glBufferData(GL_ARRAY_BUFFER, uvs.size() * sizeof(glm::vec2), &uvs[0], GL_STATIC_DRAW);
-
-	glEnableVertexAttribArray(1); // ist im Shader so hinterlegt
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, (void*)0);
-
-
-	std::vector<glm::vec3> chipVertices;
-	std::vector<glm::vec2> chipUvs;
-	std::vector<glm::vec3> chipNormals;
-	res = loadOBJ("Chip.obj", chipVertices, chipUvs, chipNormals);
-	glGenVertexArrays(1, &VertexArrayIDChip); // id wird an ein in methode erstelltes vbo object zugewiesen
-	glBindVertexArray(VertexArrayIDChip); // id wird an Objekt gebunden
-
-	glGenBuffers(1, &vertexBuffer); // vbo bekommt buffer zugewiesen
-	glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer); // buffer von typ arraybuffer wird gebunden
-	glBufferData(GL_ARRAY_BUFFER, chipVertices.size() * sizeof(glm::vec3), // groesse des Buffers in Byte
-		&chipVertices[0],
-		GL_STATIC_DRAW); //kein dynamischer Inhalt - statisch gezeichnet
-
-	glEnableVertexAttribArray(0); // 0 ist Standard -> Position in meisten Shadern
-	glVertexAttribPointer(0, // Position in shader
-		3, // Datenformat (3 Werte)
-		GL_FLOAT, // Typ
-		GL_FALSE, // normalisiert?
-		0, // sind Punkte direkt hintereinander gespeichert? ja-0 
-		(void*)0); // kann beliebigen Datentyp annehmen
-
-	glGenBuffers(1, &normalBuffer);
-	glBindBuffer(GL_ARRAY_BUFFER, normalBuffer);
-	glBufferData(GL_ARRAY_BUFFER, chipNormals.size() * sizeof(glm::vec3), &chipNormals[0], GL_STATIC_DRAW);
-
-	glEnableVertexAttribArray(2); // ist im Shader so hinterlegt
-	glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
-
-	glGenBuffers(1, &textureBuffer);
-	glBindBuffer(GL_ARRAY_BUFFER, textureBuffer);
-	glBufferData(GL_ARRAY_BUFFER, chipUvs.size() * sizeof(glm::vec2), &chipUvs[0], GL_STATIC_DRAW);
-
-	glEnableVertexAttribArray(1); // ist im Shader so hinterlegt
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, (void*)0);
+	loadChipToMemory();
 
 	createArrayForBoard();
 
-	boardArray[0][0] = 1;
-	boardArray[0][1] = 2;
-	boardArray[0][2] = 0;
+	// place chips in board (only for testing here)
+	boardArray[0][0] = PLAYER;
+	boardArray[0][1] = PLAYER;
+	boardArray[0][2] = PLAYER;
+	boardArray[0][3] = PLAYER;
+	boardArray[0][4] = PLAYER;
+	boardArray[5][0] = NPC;
+	boardArray[6][0] = NPC;
+	boardArray[7][0] = NPC;
+	boardArray[8][0] = NPC;
+
 
 	// Eventloop
 	while (!glfwWindowShouldClose(window))
@@ -353,15 +365,11 @@ int main(void)
 
 		glUniform3f(glGetUniformLocation(programID, "LightPosition_worldspace"), lightPos.x, lightPos.y, lightPos.z);
 
-		
-		drawBoard(vertices.size());
+		drawBoard();
 
-		drawChipsInBoardArray(chipVertices.size());
+		drawChipsInBoardArray();
 		
-
-		Model = Save;
 		drawCS();
-
 
 		// Swap buffers
 		glfwSwapBuffers(window);
@@ -384,4 +392,3 @@ int main(void)
 	glfwTerminate();
 	return 0;
 }
-
